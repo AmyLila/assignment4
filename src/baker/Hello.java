3366616426e1f9674844f332f29955cf9f18aa67
shakerbaker78 <bakeamym@yahoo.com>
2020-10-08 11:38:23 -0600

    Initial commit

---
+++ src/baker/Hello.java
@@ -0,0 +1,13 @@
+package baker;
+
+public class Hello {
+    public void sayHello(String name){
+        System.out.println("Hello, my name is "+name+"!");
+    }
+    public static void main(String[] args){
+        Hello hello = new Hello();
+        hello.sayHello( "Amy");
+    }
+
+
+}
