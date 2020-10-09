22db8b5ab9a2dbfbb0b8e5849065011af0116eca
MeganDLS <megan.siguenza@gmail.com>
2020-10-08 18:01:47 -0600

    adding a rely from Megan.

--- src/baker/Hello.java
+++ src/baker/Hello.java
@@ -1,12 +1,17 @@
 package baker;
 
+
 public class Hello {
+
     public void sayHello(String name){
         System.out.println("Hello, my name is "+name+"!");
     }
+
     public static void main(String[] args){
         Hello hello = new Hello();
         hello.sayHello( "Amy");
+        System.out.println("Hello Amy, my name is Megan.");
+
     }
 
 
